db1518a912ccfa49bdfcff4726fb17e7284c0869
Dana Bliss <danajackson2@gmail.com>
2020-10-28 18:20:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,5 @@
 def key_for_min_value(hash)
-  cheap = {hash[:key][0] => hash[:value][0]}
   hash.each {|item, cost|
-  if hash[:value] < cheap[:cost]
-    cheap = {hash[:item] => hash[:cost]}
-  end
+  
   }
-  return cheap[:item]
 end  
