51ab7adbc16a05e50eee061075cae477f6ecd6b9
Dana Bliss <danajackson2@gmail.com>
2020-10-28 18:00:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 def key_for_min_value(name_hash)
   answer = nil
-  if name_hash.to_a == []
+  if name_hash == {}
     return nil
   else
-    num = name_hash.to_a[0][1]
+     num = name_hash.first[1]
   end
   name_hash.each do |key, value|
     if value <= num
