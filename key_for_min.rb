2d35505f76e406495820def5c0ac07116128f588
Dana Bliss <danajackson2@gmail.com>
2020-10-28 18:27:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,10 @@
 require 'pry'
 def key_for_min_value(hash)
   cheap = hash.first
-  binding.pry
   hash.each {|item, cost|
-  
+  if cost < cheap[1]
+    cheap = [item, cost]
+  end  
   }
+  return cheap[0]
 end  
